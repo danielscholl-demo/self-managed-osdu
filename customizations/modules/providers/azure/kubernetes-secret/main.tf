65a0927bcb937e0c1a67f7936585cb97c130dd92
Daniel Scholl <dascholl@microsoft.com>
2022-03-11 09:09:41 -0600

    Updated

--- customizations/modules/providers/azure/kubernetes-secret/main.tf
+++ customizations/modules/providers/azure/kubernetes-secret/main.tf
@@ -1,7 +1,13 @@
 
+resource "time_sleep" "wait_for_install" {
+  create_duration = "60s"
+}
+
 data "kubernetes_secret" "secret" {
   metadata {
     name      = var.name
     namespace = var.namespace
   }
+
+  time_sleep = [time_sleep.wait_for_install]
 }
